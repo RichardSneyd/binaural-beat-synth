3b455910e0223ab865c58301e8c69f3aaa42c597
Richard Sneyd <richardsneyd@hotmail.com>
2023-10-03 09:27:21 +0100

    choose between brain wave types, clamp within range

--- synthesizer.js
+++ synthesizer.js
@@ -27,13 +27,22 @@ function addNewSynthesizer(synthInstances) {
         <div class="card-body">
             <h5 class="card-title">Synthesizer</h5>
             <div class="row">
+            <div class="col-md-2 form-group">
+            <label for="brainwaveType">Brainwave Type: </label>
+            <select class="form-control brainwaveType">
+                <option value="alpha">Alpha (8-14 Hz)</option>
+                <option value="beta">Beta (14-30 Hz)</option>
+                <option value="theta">Theta (4-8 Hz)</option>
+                <option value="delta">Delta (1-4 Hz)</option>
+            </select>
+        </div>
                 <div class="col-md-3 form-group">
                     <label for="leftFrequency">Left Freq (Hz): </label>
-                    <input type="number" class="form-control leftFrequency" value="300" min="1" max="1000">
+                    <input type="number" class="form-control leftFrequency" value="8" min="1" max="1000">
                 </div>
                 <div class="col-md-3 form-group">
                     <label for="rightFrequency">Right Freq (Hz): </label>
-                    <input type="number" class="form-control rightFrequency" value="310" min="1" max="1000">
+                    <input type="number" class="form-control rightFrequency" value="16" min="1" max="1000">
                 </div>
                 <div class="col-md-2 form-group">
                     <label for="leftVolume">Left Volume: </label>
@@ -89,10 +98,52 @@ class BinauralSynthesizer {
 
     this.cardDiv.querySelector('.muteButton').addEventListener('click', () => this.mute());
     this.cardDiv.querySelector('.unmuteButton').addEventListener('click', () => this.unmute());
+
+    this.brainwaveTypeInput = this.cardDiv.querySelector('.brainwaveType');
+    this.brainwaveTypeInput.addEventListener('change', () => this.updateFrequencyRange());
+  }
+
+  updateFrequencyRange() {
+    let minFreq, maxFreq;
+    switch (this.brainwaveTypeInput.value) {
+      case 'alpha':
+        minFreq = 8;
+        maxFreq = 14;
+        break;
+      case 'beta':
+        minFreq = 14;
+        maxFreq = 30;
+        break;
+      case 'theta':
+        minFreq = 4;
+        maxFreq = 8;
+        break;
+      case 'delta':
+        minFreq = 1;
+        maxFreq = 4;
+        break;
+    }
+  
+    this.leftFrequencyInput.setAttribute('min', minFreq);
+    this.leftFrequencyInput.setAttribute('max', maxFreq);
+    this.rightFrequencyInput.setAttribute('min', minFreq);
+    this.rightFrequencyInput.setAttribute('max', maxFreq);
   }
 
   setMasterTempo(tempo) {
     this.masterTempo = tempo;
+    this.updateTempoBasedOnMaster();
+  }
+
+  updateTempoBasedOnMaster() {
+    // Implement logic to adjust oscillator frequency or any other parameters based on masterTempo
+    if (this.isPlaying) {
+      const newLeftFrequency = parseFloat(this.leftFrequencyInput.value) * this.masterTempo / 120; // assuming 120 is default tempo
+      const newRightFrequency = parseFloat(this.rightFrequencyInput.value) * this.masterTempo / 120;
+
+      this.leftOscillator.frequency.setValueAtTime(newLeftFrequency, this.audioCtx.currentTime);
+      this.rightOscillator.frequency.setValueAtTime(newRightFrequency, this.audioCtx.currentTime);
+    }
   }
 
   updateParameters() {
@@ -111,6 +162,8 @@ class BinauralSynthesizer {
 
       this.leftGain.gain.setValueAtTime(this.muted ? 0 : leftVolume, this.audioCtx.currentTime);
       this.rightGain.gain.setValueAtTime(this.muted ? 0 : rightVolume, this.audioCtx.currentTime);
+
+      this.updateTempoBasedOnMaster(); // ensure that tempo adjustments get applied
     }
   }
 
@@ -159,6 +212,6 @@ class BinauralSynthesizer {
   }
 
   unmute() {
-   this.muted = false;
+    this.muted = false;
   }
 }
